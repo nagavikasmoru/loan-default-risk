eba9285f65fc69c4ece24ded6cb4d4a5e046a7c2
Naga vikas Moru <nagavikasmoru@gmail.com>
2026-06-12 13:02:48 +0530

    fix: set matplotlib Agg backend before pyplot import

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import joblib
 import shap
 import warnings
